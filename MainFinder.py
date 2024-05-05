f522283c20e50cbf10773e5c4baa939307363fdf
chatch520 <64481820+chatch520@users.noreply.github.com>
2024-05-05 14:12:55 -0600

    Add files via upload

---
+++ MainFinder.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+
+
+st.set_page_config(layout="wide", page_title="Current Month Payroll")
+
+st.markdown('''<h1 style='text-align: center; font-size: 30pt; margin-top: -20px;'>Welcome To The Disc Finder</h1>''', unsafe_allow_html=True)
