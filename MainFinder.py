50f554d83f6d5303034dd2a697c5fa847f2fd896
chatch520 <64481820+chatch520@users.noreply.github.com>
2024-05-05 14:28:58 -0600

    Add files via upload

--- MainFinder.py
+++ MainFinder.py
@@ -7,6 +7,8 @@ st.set_page_config(layout="wide", page_title="Disc Finder")
 
 st.markdown('''<h1 style='text-align: center; font-size: 30pt; margin-top: -20px;'>Welcome To The Disc Finder</h1>''', unsafe_allow_html=True)
 
+st.markdown('''<p style='text-align: left; font-size: 15pt;'>Welcome to Disc Finder! Currently in development, this app may not function as intended. The concept driving this app is simple: major corporations such as Amazon, Disney, Netflix, and Apple heavily promote subscription services. Yet, why subscribe when you can create your own media server, ensuring perpetual access to your content? The only hurdle is locating the physical discs to build your collection, a challenging task given the scarcity of physical media on platforms like Amazon. This is where Disc Finder steps in. Our app aims to scour prominent websites such as Amazon, Blu-ray.com, and eBay to locate the precise discs you seek.</p>''', unsafe_allow_html=True)
+
 with st.sidebar:
 
     st.text_input("Please Enter Series Title Here:")
