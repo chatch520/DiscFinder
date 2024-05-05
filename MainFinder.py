dc919affeaa8f78837e2d5a56b4261f204b2b3b2
chatch520 <64481820+chatch520@users.noreply.github.com>
2024-05-05 14:19:49 -0600

    Add files via upload

--- MainFinder.py
+++ MainFinder.py
@@ -3,6 +3,14 @@ import pandas as pd
 
 
 
-st.set_page_config(layout="wide", page_title="Current Month Payroll")
+st.set_page_config(layout="wide", page_title="Disc Finder")
 
 st.markdown('''<h1 style='text-align: center; font-size: 30pt; margin-top: -20px;'>Welcome To The Disc Finder</h1>''', unsafe_allow_html=True)
+
+with st.sidebar:
+
+    st.text_input("Please Enter Series Title Here:")
+
+    st.radio("Please Select Media Type:", ["Movie", "TV Series"])
+
+    st.multiselect("Please Select Which Media Format's Are Acceptable:", ["DVD", "Blu Ray", "Ultra 4K"])
